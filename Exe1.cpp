c767d7bd0cafa77084bc44e19bf0647a5b6d6a8b
mateus ribeiro <mateus@abcfarma.local>
2016-03-11 14:46:00 -0300

    Exercicios

---
+++ Exe1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+  int a,b = 0;
+
+  printf("Digite o primeiro valor: ");
+  scanf("%d", &a);
+  printf("Digite o segundo valor: ");
+  scanf("%d", &b);
+  printf("O resultado da soma é: %d\n", a+b);
+
+  system("pause");
+  
+  return 0;
+}
