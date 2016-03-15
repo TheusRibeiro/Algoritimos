cd28e733ebe6c9343d9ff23ca7504a067bba22ea
mateus ribeiro <mateus@abcfarma.local>
2016-03-15 14:59:18 -0300

    Numeos impares e pares em C

---
+++ Numeros impares e Numeros pares.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#define cMax 12
+using namespace std;
+
+int main(){
+
+	int mVetor[cMax],mVetorImp[cMax],mVetorPar[cMax],mImp = 0,mPar = 0;	
+	
+	for(int i = 0; i < cMax; i++){
+	
+	    cout<<"DIGITE O "<<( i + 1 )<<" NUMERO :";
+	    cin>>mVetor[i];
+		
+	}
+	
+	for(int i = 0; i < cMax; i++){
+	
+	   if(mVetor[i]%2 == 0){
+	   	  mVetorPar[mPar] = mVetor[i];mPar++;  	  
+	   }else{
+	   	  mVetorImp[mImp] = mVetor[i];mImp++;  	  	   	
+	   }
+		
+	}
+
+	
+	cout<<"IMPARES : ";
+	
+	for(int i = 0;i < mImp;i++){   
+
+       	cout<<mVetorImp[i]<<", ";
+		
+	}
+	
+	cout<<"\nPARES : ";
+	
+	for(int i = 0;i < mPar;i++){   
+
+       	cout<<mVetorPar[i]<<", ";
+		
+	}
+
+	cout<<"\n";
+	
+	system("pause");
+
+}
+
