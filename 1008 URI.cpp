e207419af0fe77d668a75ebe19ff8274494593b9
Theus Ribeiro <matheusohn@gmail.com>
2016-03-30 09:50:53 -0300

    Exercicio URI 1008
    Faça um programa que leia o nome de um vendedor, o seu salário fixo e o
    total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este
    vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o
    total a receber no final do mês, com duas casas decimais.

---
+++ 1008 URI.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int GH,N;
+	float NH,SALARIO;
+	
+	scanf("%d %d %f",&N,&GH,&NH);	
+		SALARIO=GH*NH;
+		
+	printf("NUMBER = %d\n",N);
+	printf("SALARY = U$ %.2f\n",SALARIO);
+		
+	return 0;
+}
