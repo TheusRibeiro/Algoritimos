01552b6a4ee4b381b987a6feef944ffc493fd9d2
Theus Ribeiro <matheusohn@gmail.com>
2016-04-04 11:20:50 -0300

    Exercicio 1019 URI
    Conversão de tempo:
    Leia um valor inteiro, que é o tempo de duração em segundos de um
    determinado evento em uma fábrica, e informe-o expresso no formato
    horas:minutos:segundos.

---
+++ 1019 URI.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    long int valor, hora, minuto, segundo;
+    scanf("%ld",&valor);
+    hora=valor/3600;
+    segundo=valor%3600;
+    minuto=segundo/60;
+    segundo=segundo%60;
+    printf("%ld:%ld:%ld\n",hora,minuto,segundo);
+    return 0;
+}
