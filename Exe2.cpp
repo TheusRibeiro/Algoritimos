5c1b42fc329b5af6c437be22674c21230a7e9ea5
Theus Ribeiro <matheusohn@gmail.com>
2016-04-11 16:52:03 -0300

    Exercicio media
    Mostrar media de três numeros

--- Exe2.cpp
+++ Exe2.cpp
@@ -1,22 +1,17 @@
-
 #include <stdio.h>
 
 int main(){
 
-  int n1, n2, n3= 0;
-  float m = 0;
-
+  int n1, n2, n3;
+  float m;
   printf("Digite o primeiro valor: ");
   scanf("%d", &n1);
   printf("Digite o segundo valor: ");
   scanf("%d", &n2);
   printf("Digite o terceiro valor: ");
   scanf("%d", &n3);
-
   m = (n1 + n2 + n3) / 3;
-
-  printf("O valor da média é: %f\n", m);
-
+  printf("O valor da média e: %f\n", m);
   return 0;
   
 }
