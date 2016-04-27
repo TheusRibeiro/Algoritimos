4ac686531ded4786dc01ea101a577bc571bfb64d
Theus Ribeiro <matheusohn@gmail.com>
2016-04-27 15:11:08 -0300

    Update Lista Linear em C++
    Correção na estrutura e identamento.

--- Atividade Maria Emilia.cpp
+++ Atividade Maria Emilia.cpp
@@ -20,30 +20,30 @@ int main(){
 	char ch; 
 
 	while (op != 5){
-	 	cout << "|           Fila Linear - Menu          |" << endl;
-	 	cout << " ---------------------------------------" << endl;
-	 	cout << "1 - Inserir na fila" << endl;
-	 	cout << "2 - Remover da fila" << endl;
-	 	cout << "3 - Pesquisar na fila" << endl;
-	 	cout << "4 - Listar a fila" << endl;
-	 	cout << "5 - Sair" << endl  << endl;
-	 	cout << "Escolha uma opcao: ";
+		cout << "|           Fila Linear - Menu          |" << endl;
+		cout << " ---------------------------------------" << endl;
+		cout << "1 - Inserir na fila"   << endl;
+		cout << "2 - Remover da fila"   << endl;
+		cout << "3 - Pesquisar na fila" << endl;
+		cout << "4 - Listar a fila"     << endl;
+		cout << "5 - Sair"     << endl  << endl;
+		cout << "Escolha uma opcao: ";
 
-	 	cin >> op;
+		cin >> op;
 
-	 	switch (op){
+		switch (op){
 
-	 		case 1: cout << "\nDigite um caractere para inserir: ";
+			case 1: cout << "\nDigite um caractere para inserir: ";
 					cin >> ch; //recebe um caractere p/ inserir
 					inserir(ch); //chama a funcoa inserir
 					break;
 			case 2: ch = remover( ); //chama a funcao remover sem parametros
-					cout << "\nCaractere removido : " << ch;
-					break;
+			cout << "\nCaractere removido : " << ch;
+			break;
 	 		case 3: pesquisar( ); //chama a funcao pesquisar sem parametros
-	 				break;
+	 		break;
 	 		case 4: listar( ); //chama a funcao pesquisar sem parametros
-	 				break;
+	 		break;
 	 		case 5: exit(1); // Finaliza programa
 
 	 		default: cout << "\nOpcao invalida." << endl;
@@ -52,17 +52,17 @@ int main(){
 }	
 
 void inserir(char ch){
- 	if (qtde == 6){ 
- 		cout << "\nFila cheia" << endl << endl;
- 		return;
- 	} if (fim == 5){
- 		fim = 0;
+	if (qtde == 6){ 
+		cout << "\nFila cheia" << endl << endl;
+		return;
+	} if (fim == 5){
+		fim = 0;
 	} else {
 		fim ++;
 	}
 
-	fila[fim] = ch; 
-	qtde++; 
+		fila[fim] = ch; 
+		qtde++; 
 }
 
 char remover(){
@@ -82,11 +82,10 @@ char remover(){
 	return ch; // Retorna o caratere removido
 }
 
-
-
 void pesquisar(){
 	int i,achou=0;
 	char ch; 
+
 	if (qtde< inicio){
 		cout << "\nFila vazia" << endl << endl;
 		return;
@@ -96,7 +95,7 @@ void pesquisar(){
 	cin >> ch;
 	for (i=inicio; i<=qtde; i++){
 		if (fila[i] == ch){
-			cout << "\nEste caractere esta na posicao: "<< i << endl << endl;
+			cout << "\nEste caractere esta na posicao: " << i << endl << endl;
 			achou++;
 		}
 
@@ -110,12 +109,12 @@ void listar(){
 	int i;
 	if (qtde < inicio){
 		cout << "\nFila vazia" << endl << endl;
-	return;
-}
-cout << "\nRelacao dos caracteres armazenados na fila linear: ";
+		return;
+	}
+	cout << "\nRelacao dos caracteres armazenados na fila linear: ";
 
 	for (i=inicio; i<=qtde; i++){
-		cout << "\t"<< fila[i];
+		cout << "\t" << fila[i];
 	}
 	cout << endl << endl;
 }
