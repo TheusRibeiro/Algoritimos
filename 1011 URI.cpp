1c219cfdc46b0d6a299ec4acba33dbc00037dc15
Theus Ribeiro <matheusohn@gmail.com>
2016-05-06 15:55:25 -0300

    Exercício 1011 URI Update

--- 1011 URI.cpp
+++ 1011 URI.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>                                                                                                                                                       
 #include <math.h>                                                                                                                                                                 
 
-int main()
-{
+int main(){
     int y,a,b,x,c;
+    
     scanf("%d%d%d",&a,&b,&c);
     x=(a+b+abs(a-b))/2;
     y=(x+c+abs(x-c))/2;
